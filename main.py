e3e11b44768bc04fd47755542dac88b0e9a20513
chanrl <richard129chan@gmail.com>
2019-09-14 16:29:01 -0700

    Update 9/14/2019

--- main.py
+++ main.py
@@ -26,23 +26,4 @@ for df in dfs:
 
 data = rp_data(dfs)
 
-# Example functions with short descriptions. Full docstring in rp_data.py file
-cols = ['RA9', 'RAA', 'RAR', 'WAA', 'WAR'] #Current working performance metrics with the script. SV% and IS% are edge cases with own functions below.
-
-# create_df('year of interest', percentile to separate higher paid pitcher group by, performance metrics specified above in cols)
-data.create_df('2019', 80, cols) # returns dataframe of the p-values and means of the performance metric of the higher paid(hp) and lower paid(lp) relief pitcher groups
-data.create_sum_df(80, cols) # same as above but for the entire data set scraped
-
-data.bootstrap('2019', 70, 'RAA', 10000) # returns bootstrapped sample distribution sampled from specified year and performance metric of interest, default at 10000 simulations
-data.bootstrap_stats() # returns values of upper and lower CI of sample mean distribution, sample distribution means of hp and lp groups
-
-data.bootstrap_sum(80, 'RAA', 10000) # same as above but samples from the entire data set scraped
-data.bootstrap_sum_stats() # returns values of upper and lower CI of sample mean distribution for the above, sample distribution means of hp and lp groups
-
-data.corr('2019', 70, 'RAA') # returns pearsons correlation coefficient for salary vs performance metric specified using the year specified and percentile to separate by
-data.corr_sum('2019', 70) # same as above but for the entire data set scraped
-data.scatter('2019', 80, 'RAA') # plots salary vs performance metric on the year specified
-
-##edge cases
-data.IS('2017', 80) #returns mean of inherited runners scored % of sample year with p values of hp/lp pitcher groups separated by percentile 
-data.SV('2018', 70) #returns mean of save opportunities converted % of sample year with p values of hp/lp pitcher groups separated by percentile
+cols = ['RA9', 'RAA', 'RAR', 'WAA', 'WAR']
